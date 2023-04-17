14c3bcfb5fda13f5316601da26a8853e80cbea0e
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-04-17 13:21:06 +0300

    feat: add a solution task queue.js

--- src/queue.js
+++ src/queue.js
@@ -13,20 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+ class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
+
 class Queue {
 
+  constructor(arr) {
+    this.arr = [];
+  }
+
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    function createList(arr){
+      let list = new ListNode(arr[0]);
+      let current = list;
+      for(let i = 1; i < arr.length; i++){
+        current.next = new ListNode(arr[i]);
+        current = current.next
+      }
+      return list
+    }
+     return createList(this.arr)
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    this.arr.push(value);
+
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    return this.arr.shift();
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
