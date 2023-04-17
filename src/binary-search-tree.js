458d8e9703c454e6baff27921e8bbdbc34b8a698
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-04-17 13:24:58 +0300

    fix: fix bag task binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -22,7 +22,7 @@ class BinarySearchTree {
      // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
 
-    return this.treeRoot = null;
+    return this.treeRoot;
   }
 
 
